19452a40588a46429b97c07967ea98baf38ca1f0
Joao Carlos da Rocha <joao.rocha800@gmail.com>
2017-10-10 09:00:58 -0300

    removendo arquivos de configuração

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page {
+
+	// variáveis 
+	
+	private $tpl;
+	private $options =  [];
+	private $defaults = [
+		"data" => []
+	];
+
+	// Metodo Construtor
+	
+	public function __construct ($opts = array()) {
+		 
+		 $this-> options = array_merge($this->defaults, $opts); 
+		// config
+		$config = array(
+			"tpl_dir" 		=> $_SERVER["DOCUMENT_ROOT"] . "/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
+			"debug"         => False // set to false to improve the speed
+		);
+
+		Tpl::configure( $config );
+
+		$this -> tpl = new Tpl; 
+
+		$this -> setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+  		
+	}
+	 
+	//Metodos gerais 
+	//
+	public function setData($data = array()) {
+
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key,$value);
+		}		
+	}
+	
+	public  function setTpl ($name,$data = array(), $returnHTML = False) {
+
+		$this->setData($data);
+		return $this->tpl->draw($name,$returnHTML);
+	}
+
+	// Metodo Destrutor 
+	// 
+
+	public function __destruct ()  {
+
+		$this->tpl->draw("footer");
+	}
+	
+
+}
+
+
+ ?>
---
+++ views-cache/header.5b1ac8b25bd867c3138b29eb3eef0fdf.rtpl.php
@@ -0,0 +1,11 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8">
+	<title>Hcode Store</title>
+	<link rel="stylesheet" href="">
+
+</head>
+<body>
+	
