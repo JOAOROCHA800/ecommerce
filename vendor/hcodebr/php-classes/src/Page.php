37d06f87614cd1918716ff501ef20f1fc6f6aed2
Joao Carlos da Rocha <joao.rocha800@gmail.com>
2017-10-11 13:48:02 -0300

    Tela de Login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,6 +11,8 @@ class Page {
 	private $tpl;
 	private $options =  [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data" => []
 	];
 
@@ -32,8 +34,8 @@ class Page {
 
 		$this -> setData($this->options["data"]);
 
-		$this->tpl->draw("header");
-  		
+		if ($this->options["header"] === true) $this->tpl->draw("header");
+		  		
 	}
 	 
 	//Metodos gerais 
@@ -56,7 +58,7 @@ class Page {
 
 	public function __destruct ()  {
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 	}
 	
 
