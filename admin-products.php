be8df4e516db2ab14c7d19ec41da163ec50671c5
Joao Carlos da Rocha <joao.rocha800@gmail.com>
2017-11-02 12:28:56 -0200

    Admim - Produtos Busca e Paginação

--- admin-products.php
+++ admin-products.php
@@ -4,17 +4,48 @@ use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Product;
 
-$app->get("/admin/products", function() {
-		
-		User::verifyLogin();
+$app->get("/admin/products", function(){
 
-		$products = Product::listALL();
+	User::verifyLogin();
 
-		$page = new PageAdmin();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-		$page->setTpl("products", [
-			"products"=>$products
+	if ($search != '') {
+
+		$pagination = Product::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Product::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/products?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
 		]);
+
+	}
+
+	$products = Product::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", [
+		"products"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);
+
 });
  
 $app->get("/admin/products/create", function() {
